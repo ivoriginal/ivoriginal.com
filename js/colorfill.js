a26f0812a85e6d30913691393ad885597bb0b71a
Ivo <ivo.vilches@gmail.com>
2021-06-24 17:35:01 +0200

    tremendo rejaulin

--- js/colorfill.js
+++
@@ -1,8 +0,0 @@
- $(window).bind('scroll', function() {
-     if ($(window).scrollTop() > 10) {
-         $('.mdl-layout__header').addClass('bg-color');
-     }
-     else {
-         $('.mdl-layout__header').removeClass('bg-color');
-     }
-});
--- js/waiting.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("waiting").onload = function() {pleasewait()};
-
-function pleasewait() {
-	document.getElementById("waiting").style.display = "none";
-}
